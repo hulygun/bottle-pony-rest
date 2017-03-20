e688a72a907fc30a48895e5d3c27dbbe187a601d
Denis Tormazov <hulygun@gmail.com>
2017-03-20 21:36:57 +0300

    fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
 setup(
     name='bottle-pony-rest',
     version='1.0',
     packages=['bpr'],
-    url='',
+    url='https://github.com/hulygun/bottle-pony-rest',
     license='MIT',
     author='hulygun',
+    requires=requires,
     author_email='hulygun@gmail.com',
-    description=''
+    description='Base Resource View for REST API via bottle framework and pony orm'
 )
